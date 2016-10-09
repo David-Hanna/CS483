b6309aa03a54ea6982453091f51d98db0813133d
David <dhanna@upei.ca>
2016-10-08 21:16:11 -0300

    Committing the rest of the OBJ file parsing code. At this point we need a solution for untracking a lot of these files.

--- CS483/CS483/main.cpp
+++ CS483/CS483/main.cpp
@@ -8,10 +8,18 @@
 //#include "ExampleGame.h"
 #include <cstdlib>
 
+#include "OBJFile.h"
+
 int main(int argc, char* argv[])
 {
 	//assignment1::ExampleGame exampleGame;
 	//return exampleGame.Run();
+
+	HeatStroke::OBJFile file(std::string("HeatStroke/Services/OBJ/test.obj"));
+	file.ParseFile();
+
+	std::cout << file.ToString();
+
 	system("pause");
 	return 0;
 }
